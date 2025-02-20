65b06e75f60172d4441ed6409333c9f78ee86973
unknown <Ryzen5 3600 RX5700@0yCIxc4qFo.0yCIxc4qFo>
2025-02-20 19:03:20 +0200

    private chat

--- static/send_message.js
+++ static/send_message.js
@@ -51,93 +51,131 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function getUserProfileList() {
-        fetch("/load-user-list")
-            .then(response => response.json())
-            .then(data => {
-                if (data.success) {
-                    const onlineUsers = data.onlineUsers;
-                    const offlineUsers = data.offlineUsers;
-                    const allUserElements = [];
+        const token = localStorage.getItem("session_token");
+
+        fetch("/load-user-list", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({token: token})
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                const currentUserId = data.userId;
+                const onlineUsers = data.onlineUsers;
+                const offlineUsers = data.offlineUsers;
+                const allUserElements = [];
                     
-                    const userListContainer = document.getElementById("userList-container")
-                    
-                    const offlineContainer = document.createElement("div");
-                    offlineContainer.id = "offline-users";
-                    let title = document.createElement("h3");
-                    title.textContent = "Оффлайн";
-                    offlineContainer.appendChild(title);
+                const userListContainer = document.getElementById("userList-container")
+                
+                const offlineContainer = document.createElement("div");
+                offlineContainer.id = "offline-users";
+                let title = document.createElement("h3");
+                title.textContent = "Оффлайн";
+                offlineContainer.appendChild(title);
     
-                    function createUserCard(username, userId, container) {
-                        let userCard = document.createElement("div");
-                        userCard.classList.add("user-card");
-                    
-                        let nameSpan = document.createElement("span");
-                        nameSpan.textContent = username;
-                        nameSpan.id = 'username';
+                function createUserCard(username, userId, container) {
+                    let userCard = document.createElement("div");
+                    userCard.classList.add("user-card");
                         
-                        let idSpan = document.createElement("span");
-                        idSpan.textContent = userId;
-                        idSpan.id = 'user-id';
-                        idSpan.style.display = "none"; 
-                    
-                        userCard.appendChild(nameSpan);
-                        userCard.appendChild(idSpan);
-
-                        allUserElements.push(userCard);
+                    let nameSpan = document.createElement("span");
+                    nameSpan.textContent = username;
+                    nameSpan.id = 'username';
+                            
+                    let idSpan = document.createElement("span");
+                    idSpan.textContent = userId;
+                    idSpan.id = 'user-id';
+                    idSpan.style.display = "none"; 
                         
-                        container.appendChild(userCard);
-                    }
-    
-                    onlineUsers.forEach(user => createUserCard(user[0], user[1], userListContainer));
+                    userCard.appendChild(nameSpan);
+                    userCard.appendChild(idSpan);
 
-                    userListContainer.appendChild(offlineContainer);
-                    
-                    offlineUsers.forEach(user => createUserCard(user[0], user[1], userListContainer));
-                    
-                    console.log(allUserElements)
-                    let userProfile = document.getElementById("user-profile")
-
-                    let messageButton = document.createElement("send-message-btn");
-                    messageButton.id = "send-message-btn";
-                    messageButton.textContent = "Написать в ЛС";
-                    messageButton.addEventListener("click", () => {
-                        alert(`Открытие ЛС с пользователем: ${clickvalue}`);
-                    });
-
-                    allUserElements.forEach(userElement => {
-                        console.log(userElement)
-                        userElement.addEventListener("click", (event) => {
-                            event.stopPropagation();
+                    allUserElements.push(userCard);
                             
-                            let usernameSpan = userElement.querySelector("#username");
-                            let clickvalue = usernameSpan.textContent; 
-                            let divvalue = userProfile.textContent; 
-
-                            if (divvalue === clickvalue && userProfile.style.display === 'block') {
-                                userProfile.style.display === 'none';
-                                userProfile.textContent = "";
-                                return;
-                            }
-                            
-                            userProfile.style.display = 'block';
-                            userProfile.textContent = clickvalue;
-                        });
-                    });
-                    
-                } else {
-                    console.error("Ошибка загрузки списка пользователей");
+                    container.appendChild(userCard);
                 }
-            })
-            .catch(error => console.error("Ошибка запроса:", error));
+    
+                onlineUsers.forEach(user => createUserCard(user[0], user[1], userListContainer));
+
+                userListContainer.appendChild(offlineContainer);
+                    
+                offlineUsers.forEach(user => createUserCard(user[0], user[1], userListContainer));
+                    
+                let userProfile = document.getElementById("user-profile");
+
+                let userNickname = document.createElement("span");
+                userNickname.id = "user-nickname";
+
+                let messageButton = document.createElement("button");
+                messageButton.id = "send-message-btn";
+                messageButton.textContent = "Написать в ЛС";
+
+                if (!userProfile.querySelector("#user-nickname")) {
+                    userProfile.appendChild(userNickname);
+                }
+
+                allUserElements.forEach(userElement => {
+                    userElement.addEventListener("click", (event) => {
+                        event.stopPropagation();
+
+                        let usernameSpan = userElement.querySelector("#username");
+                        let userId = userElement.querySelector("#user-id").textContent.trim();
+                        let clickvalue = usernameSpan.textContent;
+                        let currentNickname = userNickname.textContent; 
+
+                        if (currentNickname === clickvalue && userProfile.style.display === 'block') {
+                            userProfile.style.display = 'none';
+                            return;
+                        }
+
+                        userNickname.textContent = clickvalue;
+                        userProfile.style.display = 'block';
+                        
+                        // console.info(currentUserId)
+                        // console.info(userId)
+                        if (Number(userId) !== Number(currentUserId)) {
+                            userProfile.appendChild(messageButton);
+
+                            messageButton.addEventListener("click", () => {
+                                userProfile.style.display = 'none';
+                                openPrivateChat(currentUserId, userId);
+                            });
+                        } else {
+                            let existingButton = userProfile.querySelector("#send-message-btn");
+                            if (existingButton) {
+                                existingButton.remove();
+                            }
+                        }                        
+                    });
+                });
+            } else {
+                console.error("Ошибка загрузки списка пользователей");
+            }
+        })
+        .catch(error => console.error("Ошибка запроса:", error));
+    }
+
+    function openPrivateChat(currentUserId, chatPartnerId) {
+        fetch("/load-user-list", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({currentUserId, chatPartnerId})
+        })
+        .then(response => response.json())
+        .then(data => {});
     }
 
     document.addEventListener("click", (event) => {
+        const userProfile = document.getElementById("user-profile");
         const isUserCard = event.target.closest(".user-card"); 
-        let userProfile = document.getElementById   
+        const isUserProfile = event.target.closest("#user-profile"); 
     
-        if (!isUserCard && userProfile.style.display === 'block') { 
+        if (!isUserCard && !isUserProfile && userProfile.style.display === 'block') { 
             userProfile.style.display = 'none'; 
-            userProfile.textContent = ""; 
         }
     });
     
